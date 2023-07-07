3139b669e2d2f9d2aae8d870d8ab03dd94556b5a
Josie Lipkin <jlipkin8@gmail.com>
2023-07-07 14:18:32 -0400

    My solution.

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,36 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper>
+      <SelectForm value={value} onChange={onChange}>
+        {children}
+      </SelectForm>
+      <IconWrapper id="chevron-down"></IconWrapper>
+    </SelectWrapper>
   );
 };
+
+const SelectForm = styled.select`
+  appearance: none;
+  height: 100%;
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  padding: 12px 52px 12px 16px;
+  border-radius: 8px;
+  width: min-content;
+  font-size: 1em;
+`;
+
+const SelectWrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  top: 20%;
+  right: 10%;
+`;
 
 export default Select;
