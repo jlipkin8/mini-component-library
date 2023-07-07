2c0ca06b7d9742c8c4ad4a6bffe59d02f7a6217a
Josie Lipkin <jlipkin8@gmail.com>
2023-07-07 10:46:10 -0400

    Went with Josh's solution: use css variables

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,56 +5,66 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  let Component;
+let styles = {
+  large: {
+    '--height': 24 + 'px',
+    '--padding': 4 + 'px',
+    '--border-radius': 8 + 'px',
+  },
+  medium: {
+    '--height': 12 + 'px',
+    '--border-radius': 4 + 'px',
+    '--padding': 0 + 'px',
+  },
+  small: {
+    '--height': 8 + 'px',
+    '--border-radius': 4 + 'px',
+    '--padding': 0 + 'px',
+  },
+};
 
-  if (size === 'large') {
-    Component = LargeProgressBarWrapper;
-  } else if (size === 'medium') {
-    Component = MediumProgressBarWrapper;
-  } else if (size === 'small') {
-    Component = SmallProgressBarWrapper;
-  } else {
-    throw new Error(`Unrecognized Progress Bar size ${size}`);
-  }
+const ProgressBar = ({ value, size }) => {
+  let style = styles[size];
 
   return (
-    <Component
+    <Wrapper
+      style={style}
       role="progresssbar"
       aria-valuenow={value}
       aria-label="progress bar"
+      aria-valuemin="0"
+      aria-valuemax="100"
     >
-      <ProgressBarSpan value={value}></ProgressBarSpan>
-    </Component>
+      <BarWrapper>
+        {/* <VisuallyHidden>{value}</VisuallyHidden> */}
+        <ProgressBarSpan value={value}></ProgressBarSpan>
+      </BarWrapper>
+    </Wrapper>
   );
 };
 
-const ProgressBarWrapperBase = styled.div`
+const Wrapper = styled.div`
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: 8px;
   width: 370px;
   background-color: ${COLORS.transparentGray15};
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+  height: var(--height);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is full. */
+  overflow: hidden;
+  height: 100%;
+  width: auton;
 `;
 
 const ProgressBarSpan = styled.div`
   background-color: ${COLORS.primary};
-  border-radius: ${(props) =>
-    props.value === 100 ? 8 + 'px' : '8px 0px 0px 8px'};
+  border-radius: 8px 0px 0px 8px;
   width: ${(props) => props.value + '%'};
   height: 100%;
-`;
-
-const LargeProgressBarWrapper = styled(ProgressBarWrapperBase)`
-  padding: 4px;
-  height: 24px;
-`;
-const MediumProgressBarWrapper = styled(ProgressBarWrapperBase)`
-  height: 12px;
-  border-radius: 4px;
-`;
-const SmallProgressBarWrapper = styled(ProgressBarWrapperBase)`
-  height: 8px;
-  border-radius: 4px;
 `;
 
 export default ProgressBar;
