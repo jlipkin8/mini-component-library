d64b01558696e0833f711e37020172f708515c02
Josie Lipkin <jlipkin8@gmail.com>
2023-07-10 16:33:58 -0400

    My solution.

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,27 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--height': 24 + 'px',
+    '--font-size': 14 + 'px',
+    '--icon-size': 16 + 'px',
+    '--padding-left': 23 + 'px',
+    '--border-width': 1 + 'px',
+    iconSize: 16,
+    strokewidth: 1,
+  },
+  large: {
+    '--height': 36 + 'px',
+    '--font-size': 18 + 'px',
+    '--icon-size': 25 + 'px',
+    '--padding-left': 37 + 'px',
+    '--border-width': 2 + 'px',
+    iconSize: 24,
+    strokewidth: 2,
+  },
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +34,70 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = SIZES[size];
+  const iconSize = styles['iconSize'];
+  const strokeWidth = styles['strokeWidth'];
+
+  return (
+    <Wrapper>
+      <IconWrapper style={styles}>
+        <Icon
+          id={icon}
+          size={iconSize}
+          strokeWidth={strokeWidth}
+        ></Icon>
+      </IconWrapper>
+      <InputWrapper
+        width={width}
+        type="text"
+        id="search"
+        placeholder={placeholder}
+        style={styles}
+      ></InputWrapper>
+    </Wrapper>
+  );
 };
 
 export default IconInput;
+
+const Wrapper = styled.div`
+  position: relative;
+  font-family: Roboto;
+  width: fit-content;
+  isolation: isolate;
+`;
+
+const IconWrapper = styled.div`
+  height: var(--icon-size);
+  width: var(--icon-size);
+  position: absolute;
+  top: 10%;
+  color: ${COLORS.gray700};
+  z-index: 1;
+  ${Wrapper}:hover & {
+    color: ${COLORS.black};
+  }
+`;
+
+const InputWrapper = styled.input`
+  z-index: 2;
+  position: relative;
+  height: var(--height);
+  width: ${(props) => props.width};
+  font-size: var(--font-size);
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  border: none;
+  border-bottom: var(--border-width) solid ${COLORS.black};
+  padding-left: var(--padding-left);
+  background: none;
+
+  &:placeholder-shown {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  ${Wrapper}:hover & {
+    color: ${COLORS.black};
+  }
+`;
