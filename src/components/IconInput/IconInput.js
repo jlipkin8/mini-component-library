d376c628544bc3a89e2b598b0691268a930d1fc6
Josie Lipkin <jlipkin8@gmail.com>
2023-07-11 11:30:26 -0400

    Use rem

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -8,19 +8,19 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = {
   small: {
-    '--height': 24 + 'px',
-    '--font-size': 14 + 'px',
-    '--icon-size': 16 + 'px',
-    '--padding-left': 23 + 'px',
+    '--height': 24 / 16 + 'rem',
+    '--font-size': 14 / 16 + 'rem',
+    '--icon-size': 16 / 16 + 'rem',
+    '--padding-left': 24 / 16 + 'rem',
     '--border-width': 1 + 'px',
     iconSize: 16,
     strokewidth: 1,
   },
   large: {
-    '--height': 36 + 'px',
-    '--font-size': 18 + 'px',
-    '--icon-size': 25 + 'px',
-    '--padding-left': 37 + 'px',
+    '--height': 36 / 16 + 'rem',
+    '--font-size': 18 / 16 + 'rem',
+    '--icon-size': 25 / 16 + 'rem',
+    '--padding-left': 36 / 16 + 'rem',
     '--border-width': 2 + 'px',
     iconSize: 24,
     strokewidth: 2,
@@ -74,7 +74,9 @@ const IconWrapper = styled.div`
   height: var(--icon-size);
   width: var(--icon-size);
   position: absolute;
-  top: 10%;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
   color: ${COLORS.gray700};
   z-index: 1;
   ${Wrapper}:hover & {
@@ -94,6 +96,7 @@ const InputWrapper = styled.input`
   border-bottom: var(--border-width) solid ${COLORS.black};
   padding-left: var(--padding-left);
   background: none;
+  outline-offset: 2px;
 
   &:placeholder-shown {
     font-weight: 400;
